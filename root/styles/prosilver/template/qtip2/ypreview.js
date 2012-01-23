4be5acbd68aacfd0c4ba7c6566118c6a89c0dec3
randomessence <fliing4@yahoo.co.uk>
2012-01-23 19:06:28 +0000

    updated versiuon and script

--- root/styles/prosilver/template/qtip2/ypreview.js
+++ root/styles/prosilver/template/qtip2/ypreview.js
@@ -1,58 +1,78 @@
-$(document).ready(function() {
-  $('.ypreview').each(function() {
-    var a = $(this).attr("href").match(/watch\?v=(.+)+/);
-    a = a[1] + $.fn.qtip.nextid;
-    $(this).qtip({
-      content: $("<div />", {
-        id: a
-      }),
-      position: {
-        at: "top center",
-        my: "bottom center",
-        viewport: $(window)
-      },
-      show: {
-        event: "click",
-        solo: true,
-        effect: function() {
-          var b = this[0].style;
-          b.display = "none";
-          setTimeout(function() {
-            b.display = "block"
-          }, 1)
-        }
-      },
-      hide: "unfocus",
-      style: {
-        classes: "ui-tooltip-youtube",
-        width: 275
-      },
-      events: {
-        render: function(c, b) {
-          new YT.Player(a, {
-            playerVars: {
-              autoplay: 1,
-              enablejsapi: 1,
-              iv_load_policy: 3,
-              origin: document.location.host
-            },
-            origin: document.location.host,
-            height: 180,
-            width: 275,
-            videoId: a.substr(0, 11),
-            events: {
-              onReady: function(d) {
-                b.player = d.target
-              },
+$(document).ready(function()
+{
+   // Use each method to gain access to all youtube links
+   $('.ypreview').each(function()
+   {
+      // Grab video ID from the url
+      var videoID = $(this).attr('href').match(/watch\?v=(.+)+/);
+ 
+      /*
+       * We'll also add a unique identifier to the end so we can have multiple tooltips
+       * with the same youtube URL... awesome.
+       */
+      videoID = videoID[1] + $.fn.qtip.nextid;
+ 
+      // Create our tooltip
+      $(this).qtip(
+      {
+         // Stick in a dummy <div /> we can use to embed the player and set it's ID
+         content: $('<div />', { id: videoID }),
+         position: {
+			at: "top center",
+			my: "bottom center",
+            viewport: $(window) // Keep it on-screen at all times if possible
+         },
+         show: {
+            event: 'click', // Show it on click...
+            solo: true, // ...and hide all others when its shown
+            
+            /*
+             * Unfortunately for us, FF reloads iframe contents when any of its parents
+             * visibility is toggled, meaning we lose the player API reference on first hide.
+             * Luckily for us, setting and resetting the display property also causes the onReady
+             * event of the API palyer to fire! Woop. So we'll just reload it manually.
+             */
+            effect: function() {
+               var style = this[0].style;
+               style.display = 'none';
+               setTimeout(function() { style.display = 'block'; }, 1);
             }
-          })
-        },
-        hide: function(c, b) {
-          if (b.player && b.player.stopVideo) {
-            b.player.stopVideo()
-          }
-        }
-      }
-    }).click(false)
-  })
+         },
+         hide: 'unfocus', // Hide when the tooltip loses focus
+         style: {
+            classes: 'ui-tooltip-youtube', // Use a custom tooltip class
+            width: 275 // Set a static width
+         },
+         events: {
+            render: function(event, api) {
+               new YT.Player(videoID, {
+                  playerVars: {
+                     autoplay: 1,
+                     enablejsapi: 1,
+					 iv_load_policy: 3,
+                     origin: document.location.host
+                  },
+                  origin: document.location.host,
+                  height: 180,
+                  width: 275,
+                  videoId: videoID.substr(0, 11), // YouTube ID's are only 11 characters long :)
+                  events: {
+                     'onReady': function(e) {
+                        // Store the player in the API
+                        api.player = e.target;
+                     },
+                  }
+               });
+            },
+            hide: function(event, api){
+               // Pause the video when tooltip is hidden
+               if(api.player && api.player.stopVideo) {
+                  api.player.stopVideo();
+               }
+            }
+         }
+      })
+      // Prevent the link from being followed when we click it
+      .click(false)
+   });
 });
